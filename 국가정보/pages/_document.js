bac3e2b06617247c25fb1049db0ff42f01f5a423
권정훈 <righthun.kwon@gmail.com>
2024-04-27 01:01:34 +0900

    feat: pjt init

--- 국가정보/pages/_document.js
+++ 국가정보/pages/_document.js
@@ -1,15 +1,10 @@
-import {
-  Html,
-  Head,
-  Main,
-  NextScript,
-} from "next/document";
+import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
-    <Html lang="kr">
+    <Html lang='kr'>
       <Head>
-        <link type="image" rel="icon" href="/favicon.ico" />
+        <link type='image' rel='icon' href='/favicon.ico' />
       </Head>
       <body>
         <Main />
--- 국가정보/pages/index.js
+++ 국가정보/pages/index.js
@@ -1,22 +1,16 @@
-import { fetchCountries } from "@/api";
-import CountryList from "@/components/CountryList";
-import Searchbar from "@/components/Searchbar";
-import Head from "next/head";
+import { fetchCountries } from '@/api';
+import CountryList from '@/components/CountryList';
+import Searchbar from '@/components/Searchbar';
+import Head from 'next/head';
 
 export default function Home({ countries }) {
   return (
     <>
       <Head>
-        <title>NARAS</title>
-        <meta
-          property="og:image"
-          content="/thumbnail.png"
-        />
-        <meta property="og:title" content="NARAS" />
-        <meta
-          property="og:description"
-          content="전 세계 국가들의 정보를 확인해보세요"
-        />
+        <title>지구촌</title>
+        <meta property='og:image' content='/thumbnail.png' />
+        <meta property='og:title' content='지구촌' />
+        <meta property='og:description' content='전 세계 국가들의 정보를 확인해보세요' />
       </Head>
       <Searchbar />
       <CountryList countries={countries} />
@@ -25,10 +19,7 @@ export default function Home({ countries }) {
 }
 
 export const getStaticProps = async () => {
-  // API 호출 코드가 필요함
-
   const countries = await fetchCountries();
-  console.log("countries 데이터 불러옴");
 
   return {
     props: {
